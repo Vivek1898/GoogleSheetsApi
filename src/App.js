a210c1ac953b3c996dd6c112a6b99d49a43413a3
Vivek Singh <viveksingh27795@gmail.com>
2022-02-14 11:54:35 +0530

    Sheets Api Using Refs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
+import { TextField,Button } from '@material-ui/core';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+const nameInputRef=useRef("");
+const ageInputRef=useRef("");
+const salaryInputRef=useRef("");
+const hobbyInputRef=useRef("");
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    alert("Submitted");
+  
+  const tableData={
+    name:nameInputRef.current.value,
+    age:ageInputRef.current.value,
+    salary:salaryInputRef.current.value,
+    hobby: hobbyInputRef.current.value,
+  };
+ axios.post("https://sheet.best/api/sheets/30a996e8-6093-475f-83ef-2886a99a4e2b",tableData)
+ .then((response)=>{
+   console.log(response);
+ })
+tableData.reset()
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit={handleSubmit}> 
+     <TextField inputRef={nameInputRef} label='Name'/>
+     <TextField inputRef={ageInputRef} label='Age'/>
+     <TextField inputRef={salaryInputRef} label='Salary'/>
+     <TextField inputRef={hobbyInputRef} label='Hobby'/>
+     <Button type='submit'>Submit</Button>
+
+     </form>
     </div>
   );
 }
 
 export default App;
+
